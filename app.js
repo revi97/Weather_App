8d9112ff581bce5e2fd5305da495ef884511aad6
Revanth H S <revanthsrikantamurthy@gmail.com>
2019-09-05 01:54:58 +0530

    Using API to access weather
    Using DarkSky API to access temp, timezone and  summary of the current location.
    Used herokuapp to act as a proxy for the local host.

---
+++ app.js
@@ -0,0 +1,29 @@
+window.addEventListener('load',() =>{
+    let long;
+    let lat;
+    let temperatureDesc = document.querySelector(".temp-desc");
+    let temperatureDegree = document.querySelector(".temp-degree");
+    let locationTimezone = document.querySelector(".location-timezone")
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position =>{
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = "https://cors-anywhere.herokuapp.com/"
+            const api = `${proxy}https://api.darksky.net/forecast/736ad49462b5fb5634c0fc7cee6d9c12/${lat},${long}`;
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                const  {temperature,summary}    = data.currently;
+            
+                temperatureDegree.textContent = temperature;
+                temperatureDesc.textContent = summary;
+                locationTimezone.textContent = data.timezone;
+
+            });
+        });      
+    }
+});
